2c8cf826cfec754afc22963160f4727cbc2cca9a
Meysam Tamkin <meysam.tamkin@gmail.com>
2020-06-04 04:15:06 +0430

    Remove unused import

--- src/main/java/digital/paisley/tmt/config/BatchConfig.java
+++ src/main/java/digital/paisley/tmt/config/BatchConfig.java
@@ -20,8 +20,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.Resource;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 
 import javax.sql.DataSource;
 
